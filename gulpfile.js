0b08c3fe0613667b3d23fbcc16bbe4f4a3d77b89
so010 <stefan.otto@uninett.no>
2016-07-12 12:30:22 +0200

    removed autoreload from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,8 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect'),
   sass = require('gulp-sass'),
   nodemon = require('gulp-nodemon'),
   wiredep = require('wiredep').stream,
   concat = require('gulp-concat');
-
-gulp.task('connect',function(){
-  connect.server({
-    livereload: true,
-    host: '158.38.212.160',
-    port: 8005,
-    https: true
-  });
-});
-
-gulp.task('reload',function(){
-  gulp.src('./dist/**/*.*')
-  .pipe(connect.reload());
-});
 
 gulp.task('js-process', function(){
   gulp.src('./app/js/*.js')
@@ -48,12 +33,6 @@ gulp.task('serve', function () {
         watch   : 'server.js'
         //...add nodeArgs: ['--debug=5858'] to debug
         //..or nodeArgs: ['--debug-brk=5858'] to debug at server start
-    }).on('start', function () {
-        setTimeout(function () {
-            gulp.src('./dist/**/*.*')
-              .pipe(connect.reload());
-            console.log('reload browsers!');
-        }, 100); // wait for the server to finish loading before restarting the browsers
     });
 });
 
@@ -70,10 +49,9 @@ gulp.task('bower-dependencies', function () {
 gulp.task('watch',function(){
   gulp.watch(['./app/scss/*.scss'], ['sass']);
   gulp.watch(['./app/js/*.js'],['js-process']);
-  gulp.watch(['./dist/**/*.*'],['reload']);
   gulp.watch(['./app/**/*.html'],['html-process']);
   gulp.watch(['./app/css/*.css'],['css-process']);
 });
 
 
-gulp.task('default',['connect','watch','sass','js-process','serve','bower-dependencies','html-process']);
+gulp.task('default',['watch','sass','js-process','serve','bower-dependencies','html-process']);
