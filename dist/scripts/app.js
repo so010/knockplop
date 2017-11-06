0f9113b0aad373ae4cabe5e8e0c6eb44d61f2ebe
Stefan Otto <stefan.otto@uninett.no>
2017-11-06 15:27:05 +0100

    Fill settings dialog with magic

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -857,8 +857,115 @@ function toggleFullScreen() {
   }
 }
 
+// This is in origin from https://github.com/webrtc/samples/tree/gh-pages/src/content/devices/input-output MIT
+
 function showSettings() {
   document.getElementById('settingsWrapper').classList.remove('hidden');
+  let videoPreviewElement = document.querySelector('#videoPreviewElement');
+  let audioInputSelect = document.querySelector('select#audioSource');
+  let audioOutputSelect = document.querySelector('select#audioOutput');
+  let videoSelect = document.querySelector('select#videoSource');
+  let selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+  let stream;
+
+  function gotDevices(deviceInfos) {
+    // Handles being called several times to update labels. Preserve values.
+    console.log('gotDevices:',deviceInfos)
+    var values = selectors.map(function(select) {
+      return select.value;
+    });
+    selectors.forEach(function(select) {
+      while (select.firstChild) {
+        select.removeChild(select.firstChild);
+      }
+    });
+    for (var i = 0; i !== deviceInfos.length; ++i) {
+      var deviceInfo = deviceInfos[i];
+      var option = document.createElement('option');
+      option.value = deviceInfo.deviceId;
+      if (deviceInfo.kind === 'audioinput') {
+        option.text = deviceInfo.label ||
+            'microphone ' + (audioInputSelect.length + 1);
+        audioInputSelect.appendChild(option);
+      } else if (deviceInfo.kind === 'audiooutput') {
+        option.text = deviceInfo.label || 'speaker ' +
+            (audioOutputSelect.length + 1);
+        audioOutputSelect.appendChild(option);
+      } else if (deviceInfo.kind === 'videoinput') {
+        option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+        videoSelect.appendChild(option);
+      } else {
+        console.log('Some other kind of source/device: ', deviceInfo);
+      }
+    }
+    selectors.forEach(function(select, selectorIndex) {
+      if (Array.prototype.slice.call(select.childNodes).some(function(n) {
+        return n.value === values[selectorIndex];
+      })) {
+        select.value = values[selectorIndex];
+      }
+    });
+  }
+  navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+  // Attach audio output device to video element using device/sink ID.
+  function attachSinkId(element, sinkId) {
+    if (typeof element.sinkId !== 'undefined') {
+      element.setSinkId(sinkId)
+      .then(function() {
+        console.log('Success, audio output device attached: ' + sinkId);
+      })
+      .catch(function(error) {
+        var errorMessage = error;
+        if (error.name === 'SecurityError') {
+          errorMessage = 'You need to use HTTPS for selecting audio output ' +
+              'device: ' + error;
+        }
+        console.error(errorMessage);
+        // Jump back to first output device in the list as it's the default.
+        audioOutputSelect.selectedIndex = 0;
+      });
+    } else {
+      console.warn('Browser does not support output device selection.');
+    }
+  }
+
+  function changeAudioDestination() {
+    var audioDestination = audioOutputSelect.value;
+    attachSinkId(videoPreviewElement, audioDestination);
+  }
+
+  function gotStream(stream) {
+    videoPreviewElement.srcObject = stream;
+    // Refresh button list in case labels have become available
+    return navigator.mediaDevices.enumerateDevices();
+  }
+
+  function start() {
+    if (stream) {
+      stream.getTracks().forEach(function(track) {
+        track.stop();
+      });
+    }
+    var audioSource = audioInputSelect.value;
+    var videoSource = videoSelect.value;
+    var constraints = {
+      audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+      video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+    };
+    navigator.mediaDevices.getUserMedia(constraints).
+        then(gotStream).then(gotDevices).catch(handleError);
+  }
+
+  audioInputSelect.onchange = start;
+  audioOutputSelect.onchange = changeAudioDestination;
+  videoSelect.onchange = start;
+
+  start();
+
+  function handleError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+  }
 }
 
 function saveSettings() {
